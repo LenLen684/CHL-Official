1eb6deba5a7a912f516d9182c62bd7bc0ffd2448
llefiti <llefiti@blueskypacificgroup.com>
2026-08-17 17:31:57 -1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+    server: {
+    host: '127.0.0.1',
+    port: 3002
+  },
 })
